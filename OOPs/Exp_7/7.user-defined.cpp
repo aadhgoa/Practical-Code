8011025599df299c917632b0c875e58f2f8bf269
aadhgoa <53388828+aadhgoa@users.noreply.github.com>
2020-11-13 12:22:00 +0530

    Changed E7

--- OOPs/Exp_7/7.user-defined.cpp
+++ OOPs/Exp_7/7.user-defined.cpp
@@ -1,11 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-ostream& endLine( ostream& output){
-    return output <<"\n" <<flush;
+ostream& bell(ostream& output)
+{
+    return output<<'\a';
+}
+ostream& carriageReturn(ostream& output)
+{
+    return output<<'\r';
+}
+ostream& tab(ostream& output)
+{
+    return output<<'\t';
+}
+ostream& endLine(ostream& output)
+{
+    return output<<'\n'<<flush;
 }
 
-int main(){
-    cout<<"Testing: "<<endLine;
-    return 0;
-}
+int main()
+{
+     cout<<"testing the tab manipulator: "<<endLine <<'a'<<tab<<'b'<<tab<<'c'<<endLine;
+     cout<<"testing carriageReturn and bell:"<<endLine<<"***";
+     cout<<bell;
+     cout<<carriageReturn<<"-----"<<endLine; 
+    }
