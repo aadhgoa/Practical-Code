60b21d520fa3d99ce98e42c4c78be658fb8c023e
aadhgoa <53388828+aadhgoa@users.noreply.github.com>
2020-11-28 16:44:36 +0530

    made changes to Exp 7(DS)

--- codes/DS/Exp.7_Binary_Search_Tree.c
+++ codes/DS/Exp.7_Binary_Search_Tree.c
@@ -8,16 +8,6 @@ struct node
     struct node *left_child; // left child
 };
 
-struct node* search(struct node *root, int x)
-{
-    if(root==NULL || root->data==x) //if root->data is x then the element is found
-        return root;
-    else if(x>root->data) // x is greater, so we will search the right subtree
-        return search(root->right_child, x);
-    else //x is smaller than the data, so we will search the left subtree
-        return search(root->left_child,x);
-}
-
 //function to find the minimum value in a node
 struct node* find_minimum(struct node *root)
 {
@@ -31,13 +21,12 @@ struct node* find_minimum(struct node *root)
 //function to create a node
 struct node* new_node(int x)
 {
-    struct node *p;
-    p = malloc(sizeof(struct node));
-    p->data = x;
-    p->left_child = NULL;
-    p->right_child = NULL;
+    struct node *newNode = (struct node*) malloc (sizeof(struct node));
+    newNode->data = x;
+    newNode->left_child = NULL;
+    newNode->right_child = NULL;
 
-    return p;
+    return newNode;
 }
 
 struct node* insert(struct node *root, int x)
@@ -45,11 +34,11 @@ struct node* insert(struct node *root, int x)
     //searching for the place to insert
     if(root==NULL)
         return new_node(x);
-    else if(x>root->data) // x is greater. Should be inserted to right
+    else if(x > root->data) // x is greater. Should be inserted to right
         root->right_child = insert(root->right_child, x);
     else // x is smaller should be inserted to left
         root->left_child = insert(root->left_child,x);
-    return root;
+    //return root;
 }
 
 // funnction to delete a node
@@ -174,8 +163,10 @@ int main()
 				printf("\n");
 				break;
 			}
+			
 			case 6:
-				exit(0);
+				exit (0);	
+				
     			
     		default:{
     			printf("INVALID SELECTION.");
