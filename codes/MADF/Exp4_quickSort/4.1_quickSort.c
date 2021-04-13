0731fc40582394fa34adc4ca08414a8b6c45a1e2
aadhgoa <53388828+aadhgoa@users.noreply.github.com>
2021-04-13 11:18:25 +0530

    Updated the code of quick sort

--- codes/MADF/Exp4_quickSort/4.1_quickSort.c
+++ codes/MADF/Exp4_quickSort/4.1_quickSort.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-int partition(int arr[], int low, int high);
-void quickSort(int arr[],int low,int high);
-void swap(int *a, int *b);
-void printArray(int arr[], int size);
-
-
+void quickSort(int [], int, int);
+int Partition(int [], int, int);
+void interchange(int *, int *);
+void printArray(int [], int);
 
 int main(){
 
@@ -14,8 +12,9 @@ int main(){
     printf("Enter the size of the array: ");
     scanf("%d", &size);
 
-    printf("Enter the numbers to be sorted: ");
+    printf("Enter the numbers to be sorted \n");
     for(int i = 0; i < size; i++){
+        printf("Enter Number %d: ", i+1);
         scanf("%d", &arr[i]);
     }
 
@@ -23,35 +22,72 @@ int main(){
 
     printf("Sorted Array: ");
     printArray(arr, size);
-
     return 0;
 }
 
-int partition(int arr[], int low, int high){
-    int pivot = arr[high];
-
-    int i = low - 1;
-
-    for(int j = low; j <= high - 1; j++){
-        if(arr[j] < pivot){
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSort(int arr[],int low,int high){
+void quickSort(int arr[], int low, int high){
     if(low < high){
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high); 
+        int partition = Partition(arr, low, high);
+        quickSort(arr, low, partition -1);
+        quickSort(arr, partition + 1, high);
     }
 }
 
-void swap(int *a, int *b){
+int Partition(int arr[], int low, int high){
+    int i = low + 1;
+    int j = high;
+    int pivot = arr[low];
+    int stepCounter = 0;
+
+    while (i <= j)
+    {
+        while((arr[i]< pivot) && (i < high)){
+            i++;
+        }while (arr[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i < j){
+            printf("\n********************\n");
+            printf("Interchange done \n");
+            printf("i = %d \t j = %d \t pivot = %d\n", i, j, pivot);
+
+            for(int count = 0; count <= high; count++){
+                printf(" %d ", arr[count]); 
+            }
+            printf("\n\n");
+            
+            interchange(&arr[i], &arr[j]);
+        }else{
+            
+            printf("\n********************\n");
+            printf("NO Interchange done \n");
+            printf("i = %d \t j = %d \t pivot = %d\n", i, j, pivot);
+
+            for(int count = 0; count <= high; count++){
+                printf(" %d ", arr[count]); 
+            }
+            printf("\n\n");
+            i++;
+            }
+    }
+
+    arr[low] = arr[j];
+    arr[j] = pivot;
+
+    printf("\n********************\n");
+    printf("Interchange done \n");
+    printf("i = %d \t j = %d \t pivot = %d\n", i, j, pivot);
+
+    for(int count = 0; count <= high; count++){
+        printf(" %d ", arr[count]); 
+    }
+    printf("\n\n");
+    return j;
+}
+
+void interchange(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
